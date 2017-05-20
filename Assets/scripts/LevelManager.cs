5f51da1094b7e3ec46b1068f8a54875e3a270343
MARCIN\Marcin <michniak@yandex.com>
2017-05-20 20:34:13 +0200

    Upload project

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static void LoadLevel(string levelName){
+		Debug.Log ("Load " + levelName + " scene");
+		SceneManager.LoadScene (levelName);
+	}
+	public static void Quit(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
