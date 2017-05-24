3e4073de4b29fc0e7f47c9ebc0e5f0b7fc8e44d2
MARCIN\Marcin <michniak@yandex.com>
2017-05-24 19:33:10 +0200

    Added nice fade-in and fade-out effects on splash screen and menu

---
+++ Assets/scripts/FadeOut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+
+	public float fadeOutTime;
+
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image> ();
+		currentColor.a = 0;
+
+		InvokeRepeating ("fadeOut", 1f, 0.009f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void fadeOut(){
+		if (Time.timeSinceLevelLoad < fadeOutTime) {
+			float alphaChange = Time.deltaTime / fadeOutTime;
+			currentColor.a += alphaChange;
+			fadePanel.color = currentColor;
+		}
+	}
+}
