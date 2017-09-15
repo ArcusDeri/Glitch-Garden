aa93938fe5fbd243033aa5d1b00640d502971f8f
DESKTOP-GF9U2F7\michniaks <michniak@yandex.com>
2017-09-15 15:25:14 +0200

    Created "Gun" object for defenders to fire projectiles. Added animation state for gnome to throw his axe after four hops.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject Projectile, ProjectileParent, Gun;
+
+	private void Fire(){
+		GameObject newProjectile = Instantiate (Projectile) as GameObject;
+		newProjectile.transform.parent = ProjectileParent.transform;
+		newProjectile.transform.position = Gun.transform.position;
+	}
+}
--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -3,15 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Stone : MonoBehaviour {
-
-	private Health Health;
-	// Use this for initialization
-	void Start () {
-		Health = GetComponent<Health> ();
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
