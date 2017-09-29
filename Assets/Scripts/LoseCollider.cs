d29163faf66bb5e53b67f0880116eb8ada2f9275
DESKTOP-GF9U2F7\michniaks <michniak@yandex.com>
2017-09-29 09:12:35 +0200

    Copied shredder to the left and renamed it to Lose Collider. When attacker comes into it the lose screen is loadded. In future there should be healthbar like in bloons tower defense 5 and every attacker passing through the collider would decrease the amount of health. Maybe I will implement this.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager LevelManager;
+
+	// Use this for initialization
+	void Start () {
+		LevelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(){
+		LevelManager.LoadLevel ("03b Lose");
+	}
+}
