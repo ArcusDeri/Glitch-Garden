857c8cc52557a8ddaf6b56437ceb19ad32ebc124
DESKTOP-GF9U2F7\michniaks <michniak@yandex.com>
2017-10-02 11:56:41 +0200

    Created UI slider element which indicates time left for a level. If the time left is zero player wins the level, a message is shown with a happy sound played and next level will be loaded.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	public int ThisLevelTime;
+
+	private Slider MySlider;
+	private LevelManager MyLevelManager;
+	private AudioSource MyAudioSource;
+	private Text CompletedMessage;
+
+	// Use this for initialization
+	void Start () {
+		Setup ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateSlider ();
+	}
+
+	void Setup(){
+		MyLevelManager = GameObject.Find ("LevelManager").GetComponent<LevelManager> ();
+		CompletedMessage = GameObject.Find ("LevelCompleteMessage").GetComponent<Text> ();
+		CompletedMessage.enabled = false;
+		MyAudioSource = GameObject.Find ("MySource").GetComponent<AudioSource> ();
+
+		var slider = GameObject.Find ("SliderTime");
+		MySlider = slider.GetComponent<Slider> ();
+	}
+
+	void UpdateSlider(){
+		MySlider.value = Time.timeSinceLevelLoad / ThisLevelTime;
+		if (MySlider.value == 1f)
+			EndLevel ();
+	}
+
+	void EndLevel(){
+		CancelInvoke ("UpdateSlider");
+		MyAudioSource.Play ();
+		CompletedMessage.enabled = true;
+		Invoke ("LoadNextLevel", MyAudioSource.clip.length);
+	}
+
+	void LoadNextLevel(){
+		MyLevelManager.LoadNextLevel ();
+	}
+}
