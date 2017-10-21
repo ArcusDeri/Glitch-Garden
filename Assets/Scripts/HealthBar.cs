77ddbac705a1525e45560e2d27716eaf37105c0e
DESKTOP-GF9U2F7\michniaks <michniak@yandex.com>
2017-10-21 13:34:25 +0200

    Added healthbar for each defender and attacker.

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	private Slider MySlider;
+	private Health MyHealth;
+	private float MaxHealthPoints;
+
+	// Use this for initialization
+	void Start () {
+		MySlider = gameObject.GetComponent<Slider> ();
+		MyHealth = gameObject.GetComponentInParent<Health> ();
+		MaxHealthPoints = MyHealth.HealthPoints;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MySlider.value = MyHealth.HealthPoints / MaxHealthPoints;
+	}
+}
